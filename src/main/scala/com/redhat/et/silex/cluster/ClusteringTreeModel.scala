23a27984b999cde105dfb759435cecd06323b1d5
Erik Erlandson <eerlands@redhat.com>
2016-04-07 13:59:28 -0700

    rework use enhancement pattern

--- src/main/scala/com/redhat/et/silex/cluster/ClusteringTreeModel.scala
+++ src/main/scala/com/redhat/et/silex/cluster/ClusteringTreeModel.scala
@@ -25,16 +25,90 @@ import scala.collection.mutable
 import org.apache.spark.mllib.tree.model.{ Node, DecisionTreeModel }
 import org.apache.spark.mllib.tree.configuration.Algo.Algo
 import org.apache.spark.mllib.tree.configuration.FeatureType._
-import org.apache.spark.mllib.linalg.{Vector => SparkVector}
+import org.apache.spark.mllib.linalg.{ Vector => SparkVector }
 
-object ClusteringNode {
-  def apply(node: Node): ClusteringNode = {
-    node match {
-      case n: ClusteringNode => n
-      case _ => new ClusteringNode(node)
+package infra {
+  class ClusteringNode(self: Node) {
+    import ClusteringTreeModel.Predicate
+    import ClusteringNode._
+
+    def nodeIterator: Iterator[Node] = new Iterator[Node] {
+      val que = scala.collection.mutable.Queue(self)
+      def hasNext = !que.isEmpty
+      def next = {
+        val nxt = que.dequeue()
+        if (!nxt.isLeaf) {
+          que.enqueue(nxt.leftNode.get, nxt.rightNode.get)
+        }
+        nxt
+      }
+    }
+
+    private [infra] def rulesImpl(
+        names: PartialFunction[Int, String],
+        pstack: List[Predicate],
+        rmap: mutable.Map[Double, mutable.ArrayBuffer[Seq[Predicate]]]) {
+      if (self.isLeaf) {
+        val cat = self.predict.predict
+        if (!rmap.contains(cat)) rmap += (cat -> mutable.ArrayBuffer.empty[Seq[Predicate]])
+        rmap(cat) += pstack.reverse
+      } else {
+        val t = self.split.get.threshold
+        val f = self.split.get.feature
+        val fname = names.applyOrElse(f, (x: Int) => "f_" + x.toString)
+        self.leftNode.get
+          .rulesImpl(names, Predicate(fname, Predicate.LE, t)::pstack, rmap)
+        self.rightNode.get
+          .rulesImpl(names, Predicate(fname, Predicate.GT, t)::pstack, rmap)
+      }
+    }
+
+    def rules(names: PartialFunction[Int, String]): Map[Double, Seq[Seq[Predicate]]] = {
+      val rmap = mutable.Map.empty[Double, mutable.ArrayBuffer[Seq[Predicate]]]
+      rulesImpl(names, List.empty[Predicate], rmap)
+      rmap.toMap
+    }
+
+    def predictLeafId(features: SparkVector): Int = {
+      if (self.isLeaf) self.id else {
+        val split = self.split.get
+        if (split.featureType == Continuous) {
+          if (features(split.feature) <= split.threshold) {
+            self.leftNode.get.predictLeafId(features)
+          } else {
+            self.rightNode.get.predictLeafId(features)
+          }
+        } else {
+          if (split.categories.contains(features(split.feature))) {
+            self.leftNode.get.predictLeafId(features)
+          } else {
+            self.rightNode.get.predictLeafId(features)
+          }
+        }
+      }
     }
   }
 
+  object ClusteringNode {
+    implicit def fromNode(self: Node): ClusteringNode = new ClusteringNode(self)
+  }
+}
+
+import infra._
+
+class ClusteringTreeModel(self: DecisionTreeModel) {
+  import ClusteringTreeModel.Predicate
+  import ClusteringNode._
+
+  def predictLeafId(features: SparkVector): Int = self.topNode.predictLeafId(features)
+
+  def nodeIterator: Iterator[Node] = self.topNode.nodeIterator
+
+  def rules(names: PartialFunction[Int, String]): Map[Double, Seq[Seq[Predicate]]] =
+    self.topNode.rules(names)
+}
+
+object ClusteringTreeModel {
   case class Predicate(feature: String, op: Predicate.Op, threshold: Double) {
     override def toString = {
       val opstr = op match {
@@ -50,86 +124,7 @@ object ClusteringNode {
     case object LE extends Op
     case object GT extends Op
   }
-}
 
-// Construction is recursive on leftNode and rightNode, with a basis case where they are None:
-class ClusteringNode(node: Node) 
-    extends Node(node.id,
-                 node.predict,
-                 node.impurity,
-                 node.isLeaf,
-                 node.split,
-                 node.leftNode.map(ClusteringNode(_)),
-                 node.rightNode.map(ClusteringNode(_)),
-                 node.stats) {
-
-  import ClusteringNode.Predicate
-
-  def flatten: Seq[ClusteringNode] = {
-    if (isLeaf) List(this)
-    else List(this) ++
-      leftNode.get.asInstanceOf[ClusteringNode].flatten ++
-      rightNode.get.asInstanceOf[ClusteringNode].flatten
-  }
-
-  private def rulesImpl(
-      names: PartialFunction[Int, String],
-      pstack: List[Predicate],
-      rmap: mutable.Map[Double, mutable.ArrayBuffer[Seq[Predicate]]]) {
-    if (isLeaf) {
-      val cat = this.predict.predict
-      if (!rmap.contains(cat)) rmap += (cat -> mutable.ArrayBuffer.empty[Seq[Predicate]])
-      rmap(cat) += pstack.reverse
-    } else {
-      val t = this.split.get.threshold
-      val f = this.split.get.feature
-      val fname = names.applyOrElse(f, (x:Int) => "f" + x.toString)
-      leftNode.get.asInstanceOf[ClusteringNode]
-        .rulesImpl(names, Predicate(fname, Predicate.LE, t)::pstack, rmap)
-      rightNode.get.asInstanceOf[ClusteringNode]
-        .rulesImpl(names, Predicate(fname, Predicate.GT, t)::pstack, rmap)
-    }
-  }
-
-  def rules(names: PartialFunction[Int, String]): Map[Double, Seq[Seq[Predicate]]] = {
-    val rmap = mutable.Map.empty[Double, mutable.ArrayBuffer[Seq[Predicate]]]
-    rulesImpl(names, List.empty[Predicate], rmap)
-    rmap.toMap
-  }
-
-  def predictLeafId(features: SparkVector): Int = {
-    if (isLeaf) {
-      id
-    } else {
-      val ln = leftNode.get.asInstanceOf[ClusteringNode]
-      val rn = rightNode.get.asInstanceOf[ClusteringNode]
-      if (split.get.featureType == Continuous) {
-        if (features(split.get.feature) <= split.get.threshold) {
-          ln.predictLeafId(features)
-        } else {
-          rn.predictLeafId(features)
-        }
-      } else {
-        if (split.get.categories.contains(features(split.get.feature))) {
-          ln.predictLeafId(features)
-        } else {
-          rn.predictLeafId(features)
-        }
-      }
-    }
-  }
-}
-
-class ClusteringTreeModel(root: Node, algo: Algo)
-    extends DecisionTreeModel(ClusteringNode(root), algo) {
-  import ClusteringNode.Predicate
-
-  def predictLeafId(features: SparkVector): Int = {
-    this.topNode.asInstanceOf[ClusteringNode].predictLeafId(features)
-  }
-
-  def flatten: Seq[Node] = this.topNode.asInstanceOf[ClusteringNode].flatten
-
-  def rules(names: PartialFunction[Int, String]): Map[Double, Seq[Seq[Predicate]]] =
-    this.topNode.asInstanceOf[ClusteringNode].rules(names)
+  implicit def fromDTM(self: DecisionTreeModel): ClusteringTreeModel =
+    new ClusteringTreeModel(self)
 }
