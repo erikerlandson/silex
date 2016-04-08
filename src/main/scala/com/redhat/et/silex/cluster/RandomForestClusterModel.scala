5dad9eed0f51af49e546ee44809739bf66f175c7
Erik Erlandson <eerlands@redhat.com>
2016-04-08 15:50:43 -0700

    stubs for RandomForestCluster

---
+++ src/main/scala/com/redhat/et/silex/cluster/RandomForestClusterModel.scala
@@ -0,0 +1,44 @@
+/*
+ * This file is part of the "silex" library of helpers for Apache Spark.
+ *
+ * Copyright (c) 2016 Red Hat, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.redhat.et.silex.cluster
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.tree.model.RandomForestModel
+import org.apache.spark.mllib.linalg.{
+  Vector => SparkVec,
+  DenseVector => DenseSparkVec
+}
+
+import ClusteringRandomForestModel._
+
+class RandomForestClusterModel[T](
+  val extractor: T => Seq[Double],
+  val randomForestModel: RandomForestModel,
+  val kMedoidsModel: KMedoidsModel[Vector[Int]]) extends Serializable {
+
+  val predictor = (point: T) => {
+    // TODO: A utility that maps sequence subclasses to spark vectors with awareness of sparse
+    // formats like breeze, FeatureSeq, etc
+    val pv = new DenseSparkVec(extractor(point).toArray)
+    kMedoidsModel.predict(randomForestModel.predictLeafIds(pv))
+  }
+}
+
+object RandomForestClusterModel {
+}
