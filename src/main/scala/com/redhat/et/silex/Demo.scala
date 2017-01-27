420aa7d6206088362fc72d6bfe352ecd7c8d917a
Erik Erlandson <eerlands@redhat.com>
2017-01-27 15:51:38 -0700

    use feedgnuplot, add gaussian_mixture

--- src/main/scala/com/redhat/et/silex/Demo.scala
+++ src/main/scala/com/redhat/et/silex/Demo.scala
@@ -21,20 +21,34 @@ object demo {
   import java.io.ByteArrayInputStream
   import scala.sys.process._
 
-  def histogram[N](data: Seq[N])(implicit num: Numeric[N]): Unit = {
+  def histogram[N](data: Seq[N], cumulative: Boolean = false, normalized: Boolean = false)(implicit num: Numeric[N]): Unit = {
+    require(data.length > 0)
     val dd = data.map(num.toDouble(_))
+    val histstyle = (cumulative, normalized) match {
+      case (false, false) => "frequency"
+      case (false, true) => {
+        println("cumulative normalized histograms not supported by current gnuplot: using frequency")
+        "frequency"
+      }
+      case (true, false) => "cumulative"
+      case (true, true) => "cnormal"
+    }
+    if (cumulative) "cumulative" else "frequency"
+    val (dmin, dmax) = (dd.min, dd.max)
+    val bsden = math.min(10.0, dd.distinct.length.toDouble)
+    val binsize = (dmax-dmin)/bsden
     val inputStream: java.io.InputStream =
       new ByteArrayInputStream(dd.mkString("\n").getBytes("UTF-8"))
-    ("/usr/bin/hist" #< inputStream)!
+    (List("/usr/local/bin/feedgnuplot", "--unset", "grid", "--histogram", "0", "--histstyle", s"$histstyle", "--binwidth", s"$binsize", "--ymin", "0", "--terminal", "dumb 70,35", "--exit") #< inputStream)!
   }
 
   def scatter[N1, N2](data: Seq[(N1, N2)])(implicit
       num1: Numeric[N1], num2: Numeric[N2]): Unit = {
     val dd = data.map { case (d1, d2) => ((num1.toDouble(d1), num2.toDouble(d2))) }
-    val dc = dd.map { case (d1, d2) => s"$d1,$d2" }
+    val dc = dd.map { case (d1, d2) => s"$d1, $d2" }
     val inputStream: java.io.InputStream =
-      new ByteArrayInputStream(dc.mkString("\n").getBytes("UTF-8"))
-    ("/usr/bin/scatter" #< inputStream)!
+      new ByteArrayInputStream((dc.mkString("\n") + "\n").getBytes("UTF-8"))
+    (List("/usr/local/bin/feedgnuplot", "--points", "--terminal", "dumb 70,35", "--unset", "grid", "--domain", "--exit") #< inputStream)!
   }
 
   def scatter[N1, N2](xdata: Seq[N1], ydata: Seq[N2])(implicit
@@ -43,4 +57,17 @@ object demo {
     scatter(xdata.zip(ydata))
   }
 
+  def gaussian_mixture[N1, N2](n: Int, centers: (N1, N2)*)(implicit
+      num1: Numeric[N1], num2: Numeric[N2]): Seq[(Double, Double)] = {
+    require(n >= 0)
+    require(centers.length > 0)
+    val nc = centers.length
+    val cdd = centers.map { case (d1, d2) => ((num1.toDouble(d1), num2.toDouble(d2))) }
+    Vector.fill[(Double, Double)](n) {
+      val x = scala.util.Random.nextGaussian()
+      val y = scala.util.Random.nextGaussian()
+      val (xc, yc) = cdd(scala.util.Random.nextInt(nc))
+      (xc + x, yc + y)
+    }
+  }
 }
